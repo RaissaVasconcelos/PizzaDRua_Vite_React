17b4fe41e50b11c6c4dd2e0c71d29d96dc45efa4
Raissa <rai.celos20@gmail.com>
2023-11-11 16:16:20 -0300

    feat: update files import

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,17 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@assets/*": path.resolve(__dirname, "./src/assets/*"),
+      "@components/*": path.resolve(__dirname, "./src/components/*"),
+      "@context/*": path.resolve(__dirname, "./src/context/*"),
+      "@globais/*": path.resolve(__dirname, "./src/globais/*"),
+      "@hooks/*": path.resolve(__dirname, "./src/hooks/*"),
+      "@infrastructure/*": path.resolve(__dirname, "./src/infrastructure/*"),
+      "@layouts/*": path.resolve(__dirname, "./src/layouts/*"),
+      "@model/*": path.resolve(__dirname, "./src/model/*"),
+      "@pages/*": path.resolve(__dirname, "./src/pages/*"),
+      "@services/*": path.resolve(__dirname, "./src/services/*"),
+      "@utils/*": path.resolve(__dirname, "./src/utils/*"),
     },
   },
 })
