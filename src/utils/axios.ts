fbc1480db840c6c60dd419b95c3ab5f232a19a84
Raissa <rai.celos20@gmail.com>
2023-11-11 15:37:35 -0300

    refactor: update backend link

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,11 +2,11 @@ import axios from "axios"
 
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://hammerhead-app-mvicn.ondigitalocean.app'
 })
 
 export const socket = axios.create({
-    baseURL: 'ws://localhost:3001'
+    baseURL: 'https://hammerhead-app-mvicn.ondigitalocean.app'
 })
 
 export const jsonServer = axios.create({
